793daf2339d5e005d3bbf0244501e4f09946743e
ottopoikajarvi <tootsi666@gmail.com>
2020-04-27 16:33:52 +0300

    Initial  commit

---
+++ bluetoothserver.py
@@ -0,0 +1,67 @@
+#Modified from rfcomm-server.py Albert Huang
+#Available at: https://github.com/karulis/pybluez/blob/master/examples/simple/rfcomm-server.py
+
+from bluetooth import *
+from threading import Thread
+import sys
+import queue
+
+q = queue.Queue()
+
+
+def blueSocket(conn):
+    try:
+        while True:
+            data = conn.recv(1024)
+            if len(data) == 0:
+                break
+            print("received [%s]" % data)
+            q.put(data)
+    except IOError:
+        pass
+    print("Disconnected")
+
+def readerThread():
+    print("Printing thread started")
+    while True:
+        msg = q.get()
+        print(msg)
+
+
+def main():
+    maxcons = 4
+    worker = Thread(target=readerThread)
+    worker.start()
+
+    try:
+        for i in range(maxcons):
+            server_sock=BluetoothSocket( RFCOMM )
+            server_sock.bind(("",PORT_ANY))
+            server_sock.listen(1)
+
+            port = server_sock.getsockname()[1]
+            uuid = "d2e1c93a-887d-11ea-bc55-0242ac130003"
+
+            advertise_service( server_sock, "GymActivityServer",
+                            service_id = uuid,
+                            service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                            profiles = [ SERIAL_PORT_PROFILE ], 
+            #                   protocols = [ OBEX_UUID ] 
+                                )
+
+            print("Waiting for connection on RFCOMM channel %d" % port)
+
+            client_sock, client_info = server_sock.accept()
+            print("Accepted connection from ", client_info)
+            bluconn = Thread(target=blueSocket, args=(client_sock))
+            bluconn.start()
+        while True:
+            print("Max connections reached: " + str(maxcons))
+    except KeyboardInterrupt:
+        sys.exit()
+
+    
+    server_sock.close()
+    print("all done")
+
+main()
